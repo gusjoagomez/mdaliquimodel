165ef654de6542ca326c0f996a0208e3a8342bcf
Gustavo Gomez <gusjoagomez@hotmail.com>
2023-07-18 22:54:19 -0300

    Agregando DetalleHab corregido

--- migracion/detalleshab.go
+++ migracion/detalleshab.go
@@ -1,22 +1,38 @@
 package models
 
-import (
-	"time"
-)
-
-type EmpleadoJ50 struct {
-	Legajo            int
-	Cargo             int
-	FechaDesde        *time.Time `json:"fecha_desde" gorm:"column:fecha_desde;type:date; default:null"`
-	FechaHasta        *time.Time `json:"fecha_hasta" gorm:"column:fecha_hasta;type:date; default:null"`
-	Empresa           string     `json:"empresa" gorm:"column:empresa;type:varchar(100); default:null"`
-	ComputaParaAntig  string     `json:"computa_para_antig" gorm:"column:computa_para_antig;type:varchar(1); default:null"`
-	ComputaParaVacac  string     `json:"computa_para_vacac" gorm:"column:computa_para_vacac;type:varchar(1); default:null"`
-	FechaPresentacion *time.Time `json:"fecha_presentacion" gorm:"column:fecha_presentacion;type:date; default:null"`
-	FechaAceptacion   *time.Time `json:"fecha_aceptacion" gorm:"column:fecha_aceptacion;type:date; default:null"`
-	Procesado         string     `json:"procesado" gorm:"column:procesado;type:char(1); default:null"`
+type DetallesHab struct {
+	TipoReg                     int    //1=haberes, 2=SAC
+	Mes                         int    //1-12
+	Anio                        int    //1995-2023
+	TipoEmp                     string //D: Docentes, H: HorasCátedras, P: Prejubilados, B: Becarios, L: Limpieza (camioneros), A: Resto de empleados (planta, mensualizados, médicos)
+	Legajo                      int
+	NombreApellido              string
+	Documento                   int
+	Cuil                        int
+	GrupoLiquidacion            int
+	DescripcionGrupoLiquidacion string
+	CodigoTipoPlanta            int
+	DescripcionTipoPlanta       string
+	CodigoTipoPuesto            int
+	DescripcionTipoPuesto       string
+	CodigoGrupoPersonal         int
+	DescripcionGrupoPersonal    string
+	CodigoFuncion               int
+	DescripcionFuncion          string
+	Codep                       string
+	DescripcionCodep            string
+	CodigoConcepto              int
+	DescripcionConcepto         string
+	CodigoEntidad               int
+	DescripcionEntidad          string
+	Unidades                    float64
+	Importe                     float64
+	AnomesRetroactivo           int
+	Escalafon                   int
+	Categoria                   int
+	Descripcion                 string
 }
 
-func (e *EmpleadoJ50) TableName() string {
-	return "migracion.empleadoJ50"
+func (e *DetallesHab) TableName() string {
+	return "migracion.DetallesHab"
 }
--- migracion/empleadoJ50.go
+++ migracion/empleadoJ50.go
@@ -1,38 +1,22 @@
 package models
 
-type DetallesHab struct {
-	TipoReg                     int    //1=haberes, 2=SAC
-	Mes                         int    //1-12
-	Anio                        int    //1995-2023
-	TipoEmp                     string //D: Docentes, H: HorasCátedras, P: Prejubilados, B: Becarios, L: Limpieza (camioneros), A: Resto de empleados (planta, mensualizados, médicos)
-	Legajo                      int
-	NombreApellido              string
-	Documento                   int
-	Cuil                        int
-	GrupoLiquidacion            int
-	DescripcionGrupoLiquidacion string
-	CodigoTipoPlanta            int
-	DescripcionTipoPlanta       string
-	CodigoTipoPuesto            int
-	DescripcionTipoPuesto       string
-	CodigoGrupoPersonal         int
-	DescripcionGrupoPersonal    string
-	CodigoFuncion               int
-	DescripcionFuncion          string
-	Codep                       string
-	DescripcionCodep            string
-	CodigoConcepto              int
-	DescripcionConcepto         string
-	CodigoEntidad               int
-	DescripcionEntidad          string
-	Unidades                    float64
-	Importe                     float64
-	AnomesRetroactivo           int
-	Escalafon                   int
-	Categoria                   int
-	Descripcion                 string
+import (
+	"time"
+)
+
+type EmpleadoJ50 struct {
+	Legajo            int
+	Cargo             int
+	FechaDesde        *time.Time `json:"fecha_desde" gorm:"column:fecha_desde;type:date; default:null"`
+	FechaHasta        *time.Time `json:"fecha_hasta" gorm:"column:fecha_hasta;type:date; default:null"`
+	Empresa           string     `json:"empresa" gorm:"column:empresa;type:varchar(100); default:null"`
+	ComputaParaAntig  string     `json:"computa_para_antig" gorm:"column:computa_para_antig;type:varchar(1); default:null"`
+	ComputaParaVacac  string     `json:"computa_para_vacac" gorm:"column:computa_para_vacac;type:varchar(1); default:null"`
+	FechaPresentacion *time.Time `json:"fecha_presentacion" gorm:"column:fecha_presentacion;type:date; default:null"`
+	FechaAceptacion   *time.Time `json:"fecha_aceptacion" gorm:"column:fecha_aceptacion;type:date; default:null"`
+	Procesado         string     `json:"procesado" gorm:"column:procesado;type:char(1); default:null"`
 }
 
-func (e *DetallesHab) TableName() string {
-	return "migracion.DetallesHab"
+func (e *EmpleadoJ50) TableName() string {
+	return "migracion.empleadoJ50"
 }
