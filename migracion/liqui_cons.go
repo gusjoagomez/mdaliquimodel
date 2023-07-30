abdaf2f6c2d740697015048cc6da920a8da7b877
Gustavo Gomez <gusjoagomez@hotmail.com>
2023-07-30 00:08:18 -0300

    Modificacion liquicons decimal

--- migracion/liqui_cons.go
+++ migracion/liqui_cons.go
@@ -2,27 +2,29 @@ package models
 
 import (
 	"time"
+
+	"github.com/shopspring/decimal"
 )
 
 type LiquiCons struct {
 	//gorm.Model       // adds ID, created_at etc.
-	ID            uint       `json:"id" gorm:"primaryKey;not null;size:32;type:int4;column:id"`
-	Created_at    time.Time  `json:"created_at" gorm:"column:created_at"`
-	Updated_at    *time.Time `json:"updated_at" gorm:"column:updated_at"`
-	Deleted_at    *time.Time `json:"deleted_at" gorm:"column:deleted_at"`
-	Liqui_id      uint       `json:"liqui_id" gorm:"type:int8;column:liqui_id"`
-	Nro_legajo    uint       `json:"nro_legajo" gorm:"type:int4;column:nro_legajo"`
-	Nro_cargo     uint       `json:"nro_cargo" gorm:"type:int4;column:nro_cargo"`
-	Anio_liqui    uint       `json:"anio_liqui" gorm:"type:int4;column:anio_liqui"`
-	Anio_dev      uint       `json:"anio_dev" gorm:"type:int4;column:anio_dev"`
-	Mes_dev       uint       `json:"mes_dev" gorm:"type:int4;column:mes_dev"`
-	Fecha_desde   time.Time  `json:"fecha_desde" gorm:"column:fecha_desde"`
-	Concepto_id   uint       `json:"concepto_id" gorm:"type:int8;column:concepto_id"`
-	Cantidad      string     `json:"cantidad" gorm:"column:cantidad"`
-	Imp_unit      uint       `json:"imp_unit" gorm:"type:numeric(12,2);column:imp_unit"`
-	Imp_item      uint       `json:"imp_item" gorm:"type:numeric(12,2);column:imp_item"`
-	Tipo_concepto uint       `json:"tipo_concepto" gorm:"type:int8;column:tipo_concepto"`
-	Tipo_con_gan  uint       `json:"tipo_con_gan" gorm:"type:int8;column:tipo_con_gan"`
+	ID            uint            `json:"id" gorm:"primaryKey;not null;size:32;type:int4;column:id"`
+	Created_at    time.Time       `json:"created_at" gorm:"column:created_at"`
+	Updated_at    *time.Time      `json:"updated_at" gorm:"column:updated_at"`
+	Deleted_at    *time.Time      `json:"deleted_at" gorm:"column:deleted_at"`
+	Liqui_id      uint            `json:"liqui_id" gorm:"type:int8;column:liqui_id"`
+	Nro_legajo    uint            `json:"nro_legajo" gorm:"type:int4;column:nro_legajo"`
+	Nro_cargo     uint            `json:"nro_cargo" gorm:"type:int4;column:nro_cargo"`
+	Anio_liqui    uint            `json:"anio_liqui" gorm:"type:int4;column:anio_liqui"`
+	Anio_dev      uint            `json:"anio_dev" gorm:"type:int4;column:anio_dev"`
+	Mes_dev       uint            `json:"mes_dev" gorm:"type:int4;column:mes_dev"`
+	Fecha_desde   time.Time       `json:"fecha_desde" gorm:"column:fecha_desde"`
+	Concepto_id   uint            `json:"concepto_id" gorm:"type:int8;column:concepto_id"`
+	Cantidad      string          `json:"cantidad" gorm:"column:cantidad"`
+	Imp_unit      decimal.Decimal `json:"imp_unit" gorm:"type:numeric(12,2);column:imp_unit"`
+	Imp_item      decimal.Decimal `json:"imp_item" gorm:"type:numeric(12,2);column:imp_item"`
+	Tipo_concepto uint            `json:"tipo_concepto" gorm:"type:int8;column:tipo_concepto"`
+	Tipo_con_gan  uint            `json:"tipo_con_gan" gorm:"type:int8;column:tipo_con_gan"`
 }
 
 func (e *LiquiCons) TableName() string {
