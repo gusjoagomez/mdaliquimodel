9ad559c41ea91a7c8d08e5f3b0a7cc55d76936dc
Gustavo Gomez <gusjoagomez@hotmail.com>
2023-08-08 11:04:02 -0300

    Quita campo gruper y se agrega legajo_id

--- migracion/liqui_cons.go
+++ migracion/liqui_cons.go
@@ -13,9 +13,11 @@ type LiquiCons struct {
 	Updated_at    *time.Time      `json:"updated_at" gorm:"column:updated_at"`
 	Deleted_at    *time.Time      `json:"deleted_at" gorm:"column:deleted_at"`
 	Liqui_id      uint            `json:"liqui_id" gorm:"type:int8;column:liqui_id"`
+	Legajo_id     uint            `json:"legajo_id" gorm:"type:int4;column:legajo_id"`
 	Nro_legajo    uint            `json:"nro_legajo" gorm:"type:int4;column:nro_legajo"`
 	Nro_cargo     uint            `json:"nro_cargo" gorm:"type:int4;column:nro_cargo"`
-	Anio_liqui    uint            `json:"anio_liqui" gorm:"type:int4;column:anio_liqui"`
+	Anio_liq      uint            `json:"anio_liq" gorm:"type:int4;column:anio_liq"`
+	Mes_liq       uint            `json:"mes_liq" gorm:"type:int4;column:mes_liq"`
 	Anio_dev      uint            `json:"anio_dev" gorm:"type:int4;column:anio_dev"`
 	Mes_dev       uint            `json:"mes_dev" gorm:"type:int4;column:mes_dev"`
 	Fecha_desde   time.Time       `json:"fecha_desde" gorm:"column:fecha_desde"`
