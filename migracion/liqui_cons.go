68484760ea51ecc10b708d7b37ffad363f2439c7
Gustavo Gomez <gusjoagomez@hotmail.com>
2023-08-20 15:33:30 -0300

    Se agrega aniomes_pag y aniomes_dev en liquicons

--- migracion/liqui_cons.go
+++ migracion/liqui_cons.go
@@ -18,8 +18,10 @@ type LiquiCons struct {
 	Nro_cargo     uint            `json:"nro_cargo" gorm:"type:int4;column:nro_cargo"`
 	Anio_liq      uint            `json:"anio_liq" gorm:"type:int4;column:anio_liq"`
 	Mes_liq       uint            `json:"mes_liq" gorm:"type:int4;column:mes_liq"`
+	Aniomes_pag   string          `json:"aniomes_pag" gorm:"type:type:varchar(6);column:aniomes_pag"`
 	Anio_dev      uint            `json:"anio_dev" gorm:"type:int4;column:anio_dev"`
 	Mes_dev       uint            `json:"mes_dev" gorm:"type:int4;column:mes_dev"`
+	Aniomes_dev   string          `json:"aniomes_dev" gorm:"type:varchar(6);column:aniomes_dev"`
 	Fecha_desde   time.Time       `json:"fecha_desde" gorm:"column:fecha_desde"`
 	Concepto_id   uint            `json:"concepto_id" gorm:"type:int8;column:concepto_id"`
 	Cantidad      decimal.Decimal `json:"cantidad" gorm:"type:numeric(10,2);column:cantidad"`
